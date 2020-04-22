5b37b6330f86e9d013ae4686b54d971f09db79d6
Jo Bovy <bovy@astro.utoronto.ca>
2020-04-21 22:53:08 -0400

    Add long_description to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
     
+long_description= ''
+previous_line= ''
+with open('README.md') as dfile:
+    for line in dfile:
+        if '[!' in line: continue
+        long_description+= line
+
 setup(name='kimmy',
       version='0.1',
       description='Chemical evolution in galaxies',
+      long_description=long_description,
+      long_description_content_type='text/markdown',     
       author='Jo Bovy',
       author_email='bovy@astro.utoronto.ca',
       license='MIT',
